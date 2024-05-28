80538f9cfde0a9ce61b86d3226ee708cf1f8f4f4
Aditya Singh Bhadouria <adityabhadoria2000@gmail.com>
2024-05-28 12:57:46 +0530

    changed lens ui

--- lenskart_clone/Frontend/src/Pages/Lenses/SelectLens.jsx
+++ lenskart_clone/Frontend/src/Pages/Lenses/SelectLens.jsx
@@ -19,8 +19,21 @@ const SelectLens = ({ isOpen, onClose }) => {
   const renderLensContent = (category) => (
     <Box mt="10px">
       {lensData[category].map((item, idx) => (
-        <Box key={idx} mb="20px" display="flex" justifyContent="space-between" bg="#455666" color="white" pl={{ base: "10px", md: "20px" }} borderRadius="10px" _hover={{ transform: "scale(1.02)" }} transition="0.3s">
-          <div>
+        <Box
+          key={idx}
+          mb="20px"
+          display="flex"
+          justifyContent="space-between"
+          bg="#F5F5F5"
+          color="#333"
+          pl={{ base: "10px", md: "20px" }}
+          borderRadius="10px"
+          // _hover={{ transform: "scale(1.02)", boxShadow: "lg" }}
+          transition="0.3s"
+          boxShadow="md"
+          cursor="pointer"
+        >
+          <Box w="80%">
             <Text fontWeight="bold" fontSize={{ base: "4vw", md: "30px" }}>{item.name}</Text>
             <Box pl={{ base: "10px", md: "20px" }}>
               {item.features.map((feature, fidx) => (
@@ -28,10 +41,10 @@ const SelectLens = ({ isOpen, onClose }) => {
               ))}
             </Box>
             <Text mt="10px" fontWeight="bold" fontSize={{ base: "4vw", md: "30px" }}>{item.price}</Text>
-          </div>
-          <div>
-            <img width="200px" height="auto" src={item.src}/>
-          </div>
+          </Box>
+          <Box width="20%">
+            <img width="200px" height="auto" src={item.src} style={{ borderRadius: "10px" }}/>
+          </Box>
         </Box>
       ))}
     </Box>
@@ -41,20 +54,22 @@ const SelectLens = ({ isOpen, onClose }) => {
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
       <ModalContent
-        rounded="3xl"
+        rounded="xl"
         maxWidth={{ base: "95%", md: "80%" }}
         maxHeight={{ base: "95vh", md: "80vh" }}
         overflowY={{ base: "auto", md: "hidden" }}
         mt={{ base: "5%", md: "0" }}
+        boxShadow="2xl"
       >
-        <Flex justify="center" fontWeight="bold" fontSize={{ base: "6vw", md: "2.7vw" }} color="#455666" m="10px">
+        <Flex justify="center" fontWeight="bold" fontSize={{ base: "6vw", md: "2.2vw" }} color="#333" m="10px">
           Choose Lens Package
         </Flex>
         <ModalCloseButton
           borderRadius="50%"
           bg="white"
           m="10px 10px 0px 0px"
-          color="#455666"
+          color="#333"
+          boxShadow="md"
         />
         <ModalBody p="0px 0px" borderRadius="15px">
           <Box m={{ base: "20px", md: "34px 45px 50px 45px" }}>
@@ -70,10 +85,10 @@ const SelectLens = ({ isOpen, onClose }) => {
                   <Box key={index}>
                     <Box
                       as="button"
-                      color={selectedCategory === category ? "#455666" : "gray.400"}
+                      color={selectedCategory === category ? "#333" : "gray.500"}
                       fontWeight={selectedCategory === category ? "700" : "600"}
-                      fontSize={{ base: "5vw", md: "35px" }}
-                      _hover={{ transform: "scale(1.05)", fontWeight: "700", color: "#455666" }}
+                      fontSize={{ base: "5vw", md: "23px" }}
+                      // _hover={{ transform: "scale(1.05)", fontWeight: "700", color: "#333" }}
                       textAlign="left"
                       mt={index > 0 ? "20px" : "0"}
                       onClick={() => setSelectedCategory(category)}
